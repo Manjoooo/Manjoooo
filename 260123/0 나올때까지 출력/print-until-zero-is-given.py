ae210964b4b70bb0041b740a9b61ed04336abf1d
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-01-23 13:12:25 +0900

    Title: 0 나올때까지 출력, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260123/0 나올때까지 출력/print-until-zero-is-given.py
@@ -0,0 +1,13 @@
+
+
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+
+    print(n)
+
+#while에는 조건문을 써도 되지만 무한루프에 기본적인 표현은 True임. 
+#계속해서 새로운 값을 받기 위해서는 안에 input을 써야함. print 또한 마찬가지임. 
