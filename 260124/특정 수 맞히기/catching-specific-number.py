e61bc5908ae6d74a7881098c7ee06a35af424964
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-01-24 22:39:13 +0900

    Title: 특정 수 맞히기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260124/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,12 @@
+
+while True:
+    a = int(input())
+    if a < 25:
+        print("Higher")
+    elif a > 25:
+        print("Lower")
+    else:
+        print("Good")
+        break
+
+#while에 a==25를 쓰는 것 보다 True를 입력.
