2b9209d4c67adc0dd7b8de8dbc4eeb5f6c4335d0
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-03-06 00:07:18 +0900

    Title: 장학금, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260306/장학금/scholarship.py
@@ -0,0 +1,12 @@
+inp = input()
+arr = inp
+
+te1 = int(arr[0])
+te2 = int(arr[1])
+
+if te1 >= 90 and te2 >= 95:
+    print("100000")
+elif te1 >= 90 and te2 >= 90:
+    print("50000")
+else:
+    print("0")
