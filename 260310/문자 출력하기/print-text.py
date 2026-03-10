035d7f2353deb1662e2651139b0629f356f9724b
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-03-10 15:49:53 +0900

    Title: 문자 출력하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260310/문자 출력하기/print-text.py
+++ 260310/문자 출력하기/print-text.py
@@ -1,4 +1,8 @@
-n = str(input())
+n = input()
 
-for i in range(9):
-    print(n, end="")
+for _ in range(8):
+    print(n, end="") 
+
+
+#range는 0부터 시작하니까 8번 출력하려면 8-1 =7이 되는게 맞음 
+#그리고 굳이 문자열은 input할때 str 안해도 되는 부분
