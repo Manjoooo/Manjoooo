28076fcee801d4c4635ab5950671f7bdbfb00e11
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-03-21 15:42:16 +0900

    Title: a의 배수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260321/a의 배수/multiple-of-a.py
+++ 260321/a의 배수/multiple-of-a.py
@@ -4,8 +4,11 @@ n = int(arr[0])
 a = int(arr[1])
 
 i=1
-while i < N:
-    if i % a == 0
-     print("1")
+while i <= n:
+    if i % a == 0:
+     print(1)
     else:
-        print("0")
+        print(0)
+    i+=1
+
+#while문은 1을 더해주지 않으면 그 값 그대로 나오니까 주의
