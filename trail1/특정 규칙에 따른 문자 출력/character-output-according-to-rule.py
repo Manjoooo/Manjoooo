469b6641061496225a99db8c03dcf00feb409d5c
Manjoooo <101613131+Manjoooo@users.noreply.github.com>
2026-07-09 19:45:08 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (47ms, 16MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+# 위쪽 절반 출력
+for i in range(n):
+    # 공백 출력
+    for j in range(n-i-1):
+        print(" ", end=" ")
+    # 별 출력
+    for j in range(i+1):
+        print("@", end=" ")
+    print()
+
+# 아래쪽 절반 출력
+for i in range(1,n):
+    for j in range(n-i):
+        print("@", end=" ")
+    # 공백 출력
+    for j in range(n-i-1):
+        print(" ", end=" ")
+
+    print()
